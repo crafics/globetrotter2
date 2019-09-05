fd04ead2b1f0b8be14580b10f546a4c6475589b8
Manfred Weber <manfred.weber@gmail.com>
2019-09-05 18:24:11 +0200

    GooglePlayServices working

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+        // enables saving game progress.
+        .EnableSavedGames()
+        // registers a callback to handle game invitations received while the game is not running.
+        //.WithInvitationDelegate(<callback method>)
+        // registers a callback for turn based match notifications received while the
+        // game is not running.
+        //.WithMatchDelegate(<callback method>)
+        // requests the email address of the player be available.
+        // Will bring up a prompt for consent.
+        //.RequestEmail()
+        // requests a server auth code be generated so it can be passed to an
+        //  associated back end server application and exchanged for an OAuth token.
+        //.RequestServerAuthCode(false)
+        // requests an ID token be generated.  This OAuth token can be used to
+        //  identify the player to other services such as Firebase.
+        //.RequestIdToken()
+        .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+        SignIn();
+        
+    }
+
+    void SignIn()
+    {
+        // authenticate user:
+        Social.localUser.Authenticate((bool success) => {
+        // handle success or failure
+        Debug.Log("OKOKOK");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
